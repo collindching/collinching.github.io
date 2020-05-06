6fa0e0543d85db616a8eb251f44ab27838063c0e
Collin <collindching@gmail.com>
2020-05-06 15:39:49 -0700

    changed title of collinearity post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Why Collinearity Is A Problem In Your Regression Model",
+        "title": "How Collinearity Is Screwing Up Your Regression Model",
         "excerpt":"Unpacking a real example based on a recent marketing project Scenario walkthrough Need to build inferential model Signs were not coming out right, and as I added variables they were flipping Needed to change the values Tested variable of interest Pulled variables out, one at a time, Recap Generate a...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Why-collinearity-is-a-problem-in-your-regression-model/",
+        "url": "http://localhost:4000/How-collinearity-is-screwing-up-your-regression-model/",
         "teaser": null
       }]
