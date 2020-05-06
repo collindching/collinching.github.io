bf198211703fb1c562dfb21acc1a3eb85267465d
Collin <collindching@gmail.com>
2020-05-05 23:36:50 -0700

    update to remote theme

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "My First Post",
-        "excerpt":"This is my first post. If you can read this, yay!  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/My-First-Post/",
-        "teaser": null
-      }]
