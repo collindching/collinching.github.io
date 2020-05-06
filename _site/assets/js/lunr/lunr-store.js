65b585a7aee15cd108786daf1597831ace8128fd
Collin <collindching@gmail.com>
2020-05-06 12:31:27 -0700

    change post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "My First Post",
-        "excerpt":"This is my first post. If you can read this, yay!  ","categories": [],
+        "title": "Why Collinearity Is A Problem In Your Regression Model",
+        "excerpt":"Why collinearity is a problem in your regression model Unpacking a real example based on a recent marketing project Scenario walkthrough Need to build inferential model Signs were not coming out right, and as I added variables they were flipping Needed to change the values Tested variable of interest Pulled...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/My-First-Post/",
+        "url": "http://localhost:4000/Why-collinearity-is-a-problem-in-your-regression-model/",
         "teaser": null
       }]
