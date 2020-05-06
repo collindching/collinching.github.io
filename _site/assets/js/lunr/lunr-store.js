17e52d069898267adf6f4e6a7f370ef57e2327e0
Collin <collindching@gmail.com>
2020-05-06 00:08:20 -0700

    change to remote

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "My First Post",
+        "excerpt":"This is my first post. If you can read this, yay!  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/My-First-Post/",
+        "teaser": null
+      }]
