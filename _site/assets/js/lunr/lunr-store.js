5cfe8634f4c61cf5b4705d9012dedb1954da1096
Collin <collindching@gmail.com>
2020-05-05 23:16:34 -0700

    update site

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "My First Post",
         "excerpt":"This is my first post. If you can read this, yay!  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2020/05/04/My-First-Post.html",
+        "url": "http://localhost:4000/My-First-Post/",
         "teaser": null
       }]
