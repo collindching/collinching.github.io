5793562cf5fadf3e4eac725aeaad297333531380
Collin <collindching@gmail.com>
2020-05-09 10:39:23 -0700

    change post name and delete scss

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Why The Coefficient Signs In Your Regression Are Wrong",
-        "excerpt":"And what you can do about it Literature review: Variable selection via clustering Oh No! I Got the Wrong Sign! What Should I do? Regression signs that flip after including other predictors Correlation based feature selection with clustering for high dimensional data Choosing the Right Variables for the Right Hand...","categories": [],
+        "excerpt":"And what you can do about it Taking an example from marketing Imagine you build a model for your company’s marketing team, and in the stakeholder meeting, you present the following output: For this output, create correlated marketing data where sales volume is target variable Create two variables that are...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/why-the-coefficient-signs-in-your-regression-are-wrong/",
+        "teaser": null
+      },{
+        "title": "Why Your Regression Coefficients Have The Wrong Sign",
+        "excerpt":"And what you can do about it Literature review: Variable selection via clustering Oh No! I Got the Wrong Sign! What Should I do? Regression signs that flip after including other predictors Correlation based feature selection with clustering for high dimensional data Choosing the Right Variables for the Right Hand...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/why-your-regression-coefficients-have-the-wrong-sign/",
         "teaser": null
       }]
