be660d85fa66e8d04137988fe30a97551e64e8e1
Collin <collindching@gmail.com>
2020-05-09 10:45:24 -0700

    push to site

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Why Your Regression Coefficient Have The Wrong Sign",
         "excerpt":"And what you can do about it Taking an example from marketing Imagine you build a model for your company’s marketing team, and in the stakeholder meeting, you present the following output: For this output, create correlated marketing data where sales volume is target variable Create two variables that are...","categories": [],
         "tags": [],
-        "url": "/why-your-regression-coefficient-have-the-wrong-sign/",
+        "url": "http://localhost:4000/why-your-regression-coefficient-have-the-wrong-sign/",
         "teaser": null
       }]
