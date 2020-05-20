8b37d773f462f41a879b1532d9f73cbd16117cca
Collin <collindching@gmail.com>
2020-05-20 09:16:10 -0700

    delete thoughts from nav

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/pvals-and-vifs/",
         "teaser": null
+      },{
+        "title": "Why linear mixed models don't come with p-values",
+        "excerpt":"https://bbolker.github.io/mixedmodels-misc/glmmFAQ.html#why-doesnt-lme4-display-denominator-degrees-of-freedomp-values-what-other-options-do-i-have  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/lmm-pvals/",
+        "teaser": null
       }]
