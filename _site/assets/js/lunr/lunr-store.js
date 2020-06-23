c092fcde5a74bcd2aab9c70750749e248a738c9e
Collin <collindching@gmail.com>
2020-06-23 12:50:14 -0700

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,16 +4,4 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/regression-signs/",
         "teaser": null
-      },{
-        "title": "Comparing P-Values and VIFs",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/pvals-and-vifs/",
-        "teaser": null
-      },{
-        "title": "Why linear mixed models don't come with p-values",
-        "excerpt":"https://bbolker.github.io/mixedmodels-misc/glmmFAQ.html#why-doesnt-lme4-display-denominator-degrees-of-freedomp-values-what-other-options-do-i-have  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/lmm-pvals/",
-        "teaser": null
       }]
